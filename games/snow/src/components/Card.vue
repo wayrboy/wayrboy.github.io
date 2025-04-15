57402933dddeaa749e83facd4f432f673daec01f
zzcodeing <wayrboy123@qq.com>
2025-04-16 00:46:01 +0800

    初始化卡片

---
+++ games/snow/src/components/Card.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card">
+    <button ref="btn" @click="clickBtn">
+      <div ref="progress" class="progress"></div>
+      伐木
+    </button>
+    <h2>数量{{ num }}</h2>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useScienceStore } from "@/stores/counter";
+
+let num = ref(0)
+const btn = ref()
+const progress = ref()
+
+const science = useScienceStore()
+
+function clickBtn() {
+
+  btn.value.disabled = true;
+  progress.value.style.display = "flex"
+  let pro = 0
+  let t = setInterval(() => {
+    pro += 10
+    progress.value.style.width = `${pro}%`;
+    if (pro >= 100) {
+      pro = 0
+
+      if (!science.autoWood) {
+        clearInterval(t)
+      }
+      progress.value.style.display = "none"
+      progress.value.style.width = `0`;
+      num.value += 1;
+      btn.value.disabled = false;
+    }
+
+
+  }, 100);
+
+}
+
+</script>
+
+
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
+
+.card {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  font-size: 2rem;
+  position: relative;
+  border-radius: 5px;
+}
+
+.progress {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: flex-end;
+  align-content: stretch;
+  border: 1px solid green;
+  display: none;
+  width: 0;
+  background-color: aquamarine;
+  transition: width 0.3s;
+}
+</style>
--- games/snow/src/main.ts
+++ games/snow/src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -9,6 +9,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
